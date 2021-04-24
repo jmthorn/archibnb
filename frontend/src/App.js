8cd0cd8c2fabd3f7dca6609deba3b6d38d202f87
Jessica T <jmthorn16@gmail.com>
2021-04-24 16:47:26 -0500

    homepage done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
 import SignupFormPage from "./components/SignupFormPage";
+import Homepage from "./components/Homepage";
 import * as sessionActions from "./store/session";
 import Navigation from "./components/Navigation";
 
@@ -18,6 +19,9 @@ function App() {
       <Navigation isLoaded={isLoaded} />
       {isLoaded && (
         <Switch>
+          <Route exact path="/">
+            <Homepage />
+          </Route>
           <Route path="/signup">
             <SignupFormPage />
           </Route>
---
+++ frontend/src/components/Homepage/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import './Homepage.css';
+
+function Homepage (props) { 
+
+    return(
+        <>
+            <div className="homepage-container">
+                <form>
+
+                </form>
+                <h1>Stay</h1>
+                <h1>In</h1>
+                <h1>Style</h1>
+                <h2>Experience a stay in an architectural icon.</h2>
+            </div>
+        </>
+    )
+}
+
+export default Homepage;
