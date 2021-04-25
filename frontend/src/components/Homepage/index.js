b9e119cc4d4de3a629f0570d1c57bc551e750f53
Jessica T <jmthorn16@gmail.com>
2021-04-24 21:24:32 -0500

    homepage form built

--- frontend/src/components/Homepage/index.js
+++ frontend/src/components/Homepage/index.js
@@ -5,13 +5,25 @@ function Homepage (props) {
 
     return(
         <>
-            <form className="search-form-container">
-                <label> Location
-                    <input placeholder="Where are you going?"></input>
-                </label>
-            </form>
-            <div className="landingphoto">
-                <img src='/images/landingpage.jpg' alt ="landing-page"></img>
+            <div className="homepage-container">
+                <form className="search-form-container">
+                    <label className="input1"> Location
+                        <input placeholder="Where are you going?"></input>
+                    </label>
+                    <label className="input2"> Check In
+                        <input placeholder="Add Dates"></input>
+                    </label>
+                    <label className="input3"> Check Out
+                        <input placeholder="Add Dates"></input>
+                    </label>
+                    <label className="input4"> Guests
+                        <input placeholder="Add Guests"></input>
+                    </label>
+                    <button>Search</button>
+                </form>
+                <div className="landingphoto">
+                    <img src='/images/landingpage.jpg' alt ="landing-page"></img>
+                </div>
             </div>
         </>
     )
